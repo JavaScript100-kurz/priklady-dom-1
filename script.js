6f203a16af7482ef18d3960a45214386623e633b
Luděk Roleček <ludek@rolecek.cz>
2023-06-06 17:46:27 +0200

    Aktualizace podkladů

---
+++ script.js
@@ -0,0 +1,70 @@
+// Na stránce pomocí JavaScriptu postupně
+// proveď následující úkoly:
+
+
+// 1. Pomocí document.querySelector vyber
+// nadpis <h1> a změň mu barvu
+
+
+
+
+// 2. K jednomu z odstavců na stránce přidej v HTML
+// třídu class="odstavec". V JavaScriptu pak vyber tento
+// prvek pomocí selektoru pro třídu a odstavci:
+// - nastav velikost písma na 24px
+// - nastav nějakou další vlastnost (udělej text tučně
+//   nebo kurzívou, přidej barvu textu nebo pozadí, ...)
+
+
+
+
+// 3. Nejlepší je vybírat prvek podle id. ID by mělo
+// být unikátní pro každý prvek, takže tak přesně víme,
+// s jakým prvkem na stránce budeme manipulovat.
+// V HTML přidej k obrázku kočky id="kocka".
+// Obrázek pomocí toho id vyber a pak mu změň hodnotu
+// atributu src, kterým se nastavuje cesta k obrázku.
+// Koček je všude dost, nepotřebujeme tu další. Vyměň
+// obrázek kočky za psa. Najdeš ho ve složce images.
+
+
+
+
+// 4. Na stránce je žlutý čtvereček. Nějak ho vyber (pomocí
+// třídy nebo si k němu přidej id).
+// v CSS je připravena třída nazvaná "aktivni" a má nastavené
+// nějaké vlastnosti. Přidej tuto třídu ke čtverečku a podívej
+// se, jak se změnil.
+
+
+
+
+// 5. Dole na stránce je šedý rámeček s nápovědou. Nadpis
+// má na sobě příšerný duhový efekt. Ne, ne. Já vím, že už
+// jásáš, jak je to super, a představuješ si, kde všude to
+// použiješ. Nedělej to! Ale je mi jasné, že tenhle souboj
+// jsem prozatím prohrál.
+// Nadpis v sekci nápověda má na sobě třídu class="duhovka".
+// Udělej mi malou radost a alespoň na této stránce se toho
+// efektu zbavíme. Nadpis vyber (podle třídy nebo mu přidej id)
+// a pomocí JavaScriptu z nadpisu třídu "duhovka" odeber.
+
+
+
+
+
+// 6. Vyber druhý odstavec textu (nejprve mu přidej id)
+// a změň textový obsah odstavce na něco vtipného.
+
+
+
+
+// 7. Vyber třetí odstavec textu (opět nejprve přidej id)
+// a změň jeho textový obsah. Tentokrát nemusíš být vtipná
+// jako v předchozím příkladu, ale zato musíš do nového
+// textu zahrnout i nějaké HTML značky - udělej například
+// pár slov v nově vloženém textu tučně.
+// Budeš tedy potřebovat nahradit HTML obsah prvku.
+
+
+
